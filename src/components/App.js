501e9fb0b45c7ebdc3c758563c6eac145f3dce96
Elena <elena.shxx@gmail.com>
2022-06-12 15:53:19 +0300

    Состояние попапов

--- src/components/App.js
+++ src/components/App.js
@@ -3,49 +3,61 @@ import Main from './Main.js';
 import Footer from './Footer.js';
 import PopupWithForm from './PopupWithForm.js';
 import ImagePopup from './ImagePopup.js';
+import React from 'react';
 
 
 
-function App() {
-    return (
-        <div className="App">
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditProfilePopupOpen: false,
+            isAddPlacePopupOpen: false,
+            isEditAvatarPopupOpen: false,
+        }
+    }
+    render() {
+        return (
+            <div className="App">
 
-            <div className="page">
-                <Header />
-                <Main />
-                <Footer />
-                <PopupWithForm handleFormSubmit="" title="Редактировать профиль" name="edit-profile" isOpened="true">
-                    <input id="name-input" type="text" className="popup__input popup__input_form-name"
-                        placeholder="Ваше имя" name="username" required minLength="2" maxLength="40" value="" />
-                    <span className="popup__type-input-error name-input-error" />
-                    <input id="about-input" type="text" className="popup__input popup__input_form-about"
-                        placeholder="Расскажите о себе..." name="about" required minLength="2" maxLength="200" value="" />
-                    <span className="popup__type-input-error about-input-error"></span>
-                </PopupWithForm>
+                <div className="page">
+                    <Header />
+                    <Main />
+                    <Footer />
+                    <PopupWithForm handleFormSubmit="" title="Редактировать профиль" name="edit-profile" isOpened={this.state.isEditProfilePopupOpen}>
+                        <input id="name-input" type="text" className="popup__input popup__input_form-name"
+                            placeholder="Ваше имя" name="username" required minLength="2" maxLength="40" value="" />
+                        <span className="popup__type-input-error name-input-error" />
+                        <input id="about-input" type="text" className="popup__input popup__input_form-about"
+                            placeholder="Расскажите о себе..." name="about" required minLength="2" maxLength="200" value="" />
+                        <span className="popup__type-input-error about-input-error"></span>
+                    </PopupWithForm>
 
-                <PopupWithForm handleFormSubmit="" title="Новое место" name="add-photo">
-                    <input id="photo-input" type="text" class="popup__input popup__input_form-title"
-                        placeholder="Описание фото" name="name" required minLength="2" maxLength="30" value="" />
-                    <span class="popup__type-input-error photo-input-error"></span>
-                    <input id="link-input" type="url" class="popup__input popup__input_form-link"
-                        placeholder="Ссылка на фото" name="link" required value="" />
-                    <span class="popup__type-input-error link-input-error"></span>
-                </PopupWithForm>
+                    <PopupWithForm handleFormSubmit="" title="Новое место" name="add-photo" isOpened={this.state.isAddPlacePopupOpen}>
+                        <input id="photo-input" type="text" className="popup__input popup__input_form-title"
+                            placeholder="Описание фото" name="name" required minLength="2" maxLength="30" value="" />
+                        <span className="popup__type-input-error photo-input-error"></span>
+                        <input id="link-input" type="url" className="popup__input popup__input_form-link"
+                            placeholder="Ссылка на фото" name="link" required value="" />
+                        <span className="popup__type-input-error link-input-error"></span>
+                    </PopupWithForm>
 
-                <PopupWithForm handleFormSubmit="" title="Обновить аватар" name="update-avatar">
-                    <input id="avatar-link-input" type="url" class="popup__input popup__input_form-link"
-                        placeholder="Ссылка на фото" name="link" required value="" />
-                    <span class="popup__type-input-error avatar-link-input-error"></span>
-                </PopupWithForm>
+                    <PopupWithForm handleFormSubmit="" title="Обновить аватар" name="update-avatar" isOpened={this.state.isEditAvatarPopupOpen}>
+                        <input id="avatar-link-input" type="url" className="popup__input popup__input_form-link"
+                            placeholder="Ссылка на фото" name="link" required value="" />
+                        <span className="popup__type-input-error avatar-link-input-error"></span>
+                    </PopupWithForm>
 
-                <PopupWithForm handleFormSubmit="" title="Вы уверены?" name="popup-delete" />
+                    <PopupWithForm handleFormSubmit="" title="Вы уверены?" name="popup-delete" />
 
-                <ImagePopup />
+                    <ImagePopup />
 
-                
+
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default App;
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -23,12 +23,12 @@ class ImagePopup extends React.Component {
         const classesList = this.state.popupIsOpened ? "popup popup_opened" : "popup";
         return (
             <section className={classesList}>
-                <div class="popup__preview">
-                    <img class="popup__img" src={this.props.src} />
-                    <p class="popup__text"></p>
-                    <button type="reset" class="popup__close-form popup__close-form_preview" aria-label>
+                <div className="popup__preview">
+                    <img className="popup__img" src={this.props.src} />
+                    <p className="popup__text"></p>
+                    <button type="reset" className="popup__close-form popup__close-form_preview" aria-label>
                         <img src={imageClose} alt="Кнопка закрыть крестик"
-                            class="popup__close-form-img" />
+                            className="popup__close-form-img" />
                     </button>
                 </div>
             </section>
