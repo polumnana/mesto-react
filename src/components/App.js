aa0fcb418c29f4382b60d2e2dd07c715391faf82
Elena <elena.shxx@gmail.com>
2022-06-12 15:27:37 +0300

    Children для всех попапов (инпуты)

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
                 <Header />
                 <Main />
                 <Footer />
-                <PopupWithForm handleFormSubmit="" title="Редактировать профиль" name="edit-profile" isOpened="true">
+                <PopupWithForm handleFormSubmit="" title="Редактировать профиль" name="edit-profile" isOpened="true" >
                     <input id="name-input" type="text" className="popup__input popup__input_form-name"
                         placeholder="Ваше имя" name="username" required minLength="2" maxLength="40" value="" />
                     <span className="popup__type-input-error name-input-error" />
@@ -22,8 +22,21 @@ function App() {
                     <span className="popup__type-input-error about-input-error"></span>
                 </PopupWithForm>
 
-                <PopupWithForm handleFormSubmit="" title="Новое место" name="add-photo"  />
-                <PopupWithForm handleFormSubmit="" title="Обновить аватар" name="update-avatar" />
+                <PopupWithForm handleFormSubmit="" title="Новое место" name="add-photo">
+                    <input id="photo-input" type="text" class="popup__input popup__input_form-title"
+                        placeholder="Описание фото" name="name" required minLength="2" maxLength="30" value="" />
+                    <span class="popup__type-input-error photo-input-error"></span>
+                    <input id="link-input" type="url" class="popup__input popup__input_form-link"
+                        placeholder="Ссылка на фото" name="link" required value="" />
+                    <span class="popup__type-input-error link-input-error"></span>
+                </PopupWithForm>
+
+                <PopupWithForm handleFormSubmit="" title="Обновить аватар" name="update-avatar">
+                    <input id="avatar-link-input" type="url" class="popup__input popup__input_form-link"
+                        placeholder="Ссылка на фото" name="link" required value="" />
+                    <span class="popup__type-input-error avatar-link-input-error"></span>
+                </PopupWithForm>
+
                 <PopupWithForm handleFormSubmit="" title="Вы уверены?" name="popup-delete" />
             </div>
         </div>
