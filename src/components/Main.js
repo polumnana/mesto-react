838fa45f5c58af7195e36d281bcfbadf21884864
Elena <elena.shxx@gmail.com>
2022-06-12 11:52:06 +0300

    Привязала кнопки в Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,42 +3,50 @@ import buttonAddPost from '../images/Add_post.svg';
 import avatar from '../images/Avatar.jpg';
 import React from 'react';
 
+const formSubmitUpdateAvatar = document.querySelector('.popup__form_update-avatar'); // Форма
+
 class Main extends React.Component {
-    // openPopupAvatar() {
-    //     formSubmitUpdateAvatar.reset();
+    openPopupAvatar() {
+        formSubmitUpdateAvatar.reset();
 
-    //     const validatorUpdateAvatar = formValidators[formSubmitUpdateAvatar.name];
-    //     validatorUpdateAvatar.clearErrors();
-    //     validatorUpdateAvatar.setFormButtonState();
+        // const validatorUpdateAvatar = formValidators[formSubmitUpdateAvatar.name];
+        // validatorUpdateAvatar.clearErrors();
+        // validatorUpdateAvatar.setFormButtonState();
 
-    //     popupUpdateAvatar.open();
-    // }
+        // popupUpdateAvatar.open();
+        console.log(1);
+    }
 
-    // openPopupEditProfile() {
+    openPopupEditProfile() {
 
-    //     inputNameEditProfile.value = userInfo.getUserInfo().name; // В инпут берутся данные из профиля
-    //     inputAboutEditProfile.value = userInfo.getUserInfo().about; // В инпут берутся данные из профиля
+        // inputNameEditProfile.value = userInfo.getUserInfo().name; // В инпут берутся данные из профиля
+        // inputAboutEditProfile.value = userInfo.getUserInfo().about; // В инпут берутся данные из профиля
 
-    //     formValidators[formSubmitEditProfile.name].clearErrors();
+        // formValidators[formSubmitEditProfile.name].clearErrors();
 
-    //     popupEditProfile.open(); // Открыть попап
-    // }
+        // popupEditProfile.open(); // Открыть попап
+        console.log(2);
 
-    // openPopupAddPost() {
-    //     formSubmitAddPost.reset();
+    }
 
-    //     const validatorAddPost = formValidators[formSubmitAddPost.name];
-    //     validatorAddPost.clearErrors();
-    //     validatorAddPost.setFormButtonState();
+    openPopupAddPost() {
+        // formSubmitAddPost.reset();
 
-    //     popupAddPost.open();
-    // }
+        // const validatorAddPost = formValidators[formSubmitAddPost.name];
+        // validatorAddPost.clearErrors();
+        // validatorAddPost.setFormButtonState();
+
+        // popupAddPost.open();
+        console.log(3);
+
+    }
 
     render() {
         return (
             <main class="content">
                 <section className="profile">
-                    <button type="button" className="profile__button-avatar" aria-label>
+                    <button type="button" className="profile__button-avatar" aria-label
+                    onClick={this.openPopupAvatar}>
                         <img src={avatar} alt="Аватар пользователя" className="profile__avatar" />
                         <div className="profile__overlay">
                             <img src={buttonEdit} alt="Кнопка карандашик" className="profile__edit-avatar" />
@@ -46,12 +54,14 @@ class Main extends React.Component {
                     </button>
                     <div className="profile__info">
                         <h1 className="profile__info-name">Жак-Ив Кусто</h1>
-                        <button type="button" className="profile__button-edit" aria-label>
-                            <img src={buttonEdit} alt="Кнопка карандашик" className />
+                        <button type="button" className="profile__button-edit" aria-label
+                            onClick={this.openPopupEditProfile}>
+                                <img src={buttonEdit} alt="Кнопка карандашик" className />
                         </button>
                     </div>
                     <p className="profile__info-about">Исследователь океана</p>
-                    <button type="button" className="profile__button-add" aria-label>
+                    <button type="button" className="profile__button-add" aria-label
+                    onClick={this.openPopupAddPost}>
                         <img src={buttonAddPost} alt="Кнопка плюсик" className />
                     </button>
                 </section>
@@ -81,9 +91,5 @@ class Main extends React.Component {
         );
     }
 }
-
-// buttonEditAvatar.addEventListener("click", openPopupAvatar);
-// buttonEditProfile.addEventListener("click", openPopupEditProfile);
-// buttonAddNewPost.addEventListener("click", openPopupAddPost);
 
 export default Main;
