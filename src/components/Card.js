9b00f6da9e2151c4f6e3993692b298d6656c43f1
Elena <elena.shxx@gmail.com>
2022-06-26 20:39:01 +0300

    Cards likes

--- src/components/Card.js
+++ src/components/Card.js
@@ -23,13 +23,20 @@ class Card extends React.Component {
         const cardDeleteButtonClassName = (
             `${isOwn ? 'element__delete' : 'element__delete_hidden'}`
         );
+        // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+        const isLiked = this.props.card.likes.some(i => i._id === this.context._id);
+
+        // Создаём переменную, которую после зададим в `className` для кнопки лайка
+        const cardLikeButtonClassName = (
+            `${isLiked ? 'element__button-like element__button-like_active' : 'element__button-like'}`
+        );
 
         return (
             <article className="element">
                 <img onClick={this.handleCardClick} className="element__img" src={this.props.card.link}/>
                 <h2 className="element__title">{this.props.card.name}</h2>
                 <div className="element__like">
-                    <button type="button" className="element__button-like" aria-label></button>
+                    <button type="button" className={cardLikeButtonClassName} aria-label></button>
                     <span className="element__counter-like">{this.props.card.likes.length}</span>
                 </div>
                 <button type="button" className={cardDeleteButtonClassName} aria-label>
